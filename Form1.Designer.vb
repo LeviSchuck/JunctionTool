906832e6a74a98a8a919212b206d3c17769ca110
Levi Schuck <kloplop321@gmail.com>
2012-08-01 20:09:24 -0600

    Working project

---
+++ Form1.Designer.vb
@@ -0,0 +1,47 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "junction.exe"
+        Me.OpenFileDialog1.Filter = "Junction Program|junction.exe"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 122)
+        Me.Name = "Form1"
+        Me.Text = "Junction Tool"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.IO
+Public Class Form1
+    Const failmessage = "You failed, please try the junction tool again"
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            'GoTo doexe
+        
+        Dim first As String = ""
+        Dim second As String = ""
+        FolderBrowserDialog1.Description = "Please select the existing folder"
+        If FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            first = FolderBrowserDialog1.SelectedPath
+            FolderBrowserDialog1.Reset()
+            FolderBrowserDialog1.Description = "Please select the destination folder"
+            If FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                second = FolderBrowserDialog1.SelectedPath
+                    If Directory.Exists(second) Then
+                        Directory.Delete(second, True)
+                    End If
+                Else
+                    MsgBox(failmessage)
+                    Throw New Exception("Failure")
+                End If
+            Else
+                MsgBox(failmessage)
+                Throw New Exception("Failure")
+            End If
+            Dim junctionpath As String = ""
+            OpenFileDialog1.Title = "Please locate the junction application"
+            If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                junctionpath = OpenFileDialog1.FileName
+            Else
+                Throw New Exception("Failure")
+            End If
+doexe:
+            Dim args As String = """" & second & """ """ & first & """"
+            Dim pi As New ProcessStartInfo("cmd.exe", "/C junction.exe " & args) '(junctionpath, args)
+            Dim output As String = ""
+            Dim p As New Process
+            pi.UseShellExecute = False
+            pi.WorkingDirectory = junctionpath.Replace("junction.exe", "")
+            pi.RedirectStandardOutput = True
+            p.StartInfo = pi
+
+
+            If p.Start() Then
+                output = p.StandardOutput.ReadToEnd()
+                p.WaitForExit()
+                MsgBox(output)
+            Else
+                MsgBox("Failure Starting")
+            End If
+            Application.Exit()
+        Catch ex As Exception
+            MsgBox(ex.ToString())
+            Application.Exit()
+        End Try
+    End Sub
+End Class
