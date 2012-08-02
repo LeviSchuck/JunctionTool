f2831934468655f330400d1dc24083e12f155695
Levi Schuck <kloplop321@gmail.com>
2012-08-01 20:22:48 -0600

    Made GUI more friendly

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,6 +24,9 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'OpenFileDialog1
@@ -31,11 +34,45 @@ Partial Class Form1
         Me.OpenFileDialog1.FileName = "junction.exe"
         Me.OpenFileDialog1.Filter = "Junction Program|junction.exe"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(190, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Choose Existing"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(13, 42)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(190, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Choose Target"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(13, 72)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(190, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Make Changes"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(249, 122)
+        Me.ClientSize = New System.Drawing.Size(215, 122)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "Junction Tool"
         Me.ResumeLayout(False)
@@ -43,5 +80,8 @@ Partial Class Form1
     End Sub
     Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
     Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,38 +1,33 @@
 ﻿Imports System.IO
 Public Class Form1
     Const failmessage = "You failed, please try the junction tool again"
+    Dim first As String = ""
+    Dim second As String = ""
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Try
-            'GoTo doexe
-        
-        Dim first As String = ""
-        Dim second As String = ""
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         FolderBrowserDialog1.Description = "Please select the existing folder"
         If FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
             first = FolderBrowserDialog1.SelectedPath
-            FolderBrowserDialog1.Reset()
-            FolderBrowserDialog1.Description = "Please select the destination folder"
-            If FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
-                second = FolderBrowserDialog1.SelectedPath
-                    If Directory.Exists(second) Then
-                        Directory.Delete(second, True)
-                    End If
-                Else
-                    MsgBox(failmessage)
-                    Throw New Exception("Failure")
-                End If
-            Else
-                MsgBox(failmessage)
-                Throw New Exception("Failure")
-            End If
-            Dim junctionpath As String = ""
-            OpenFileDialog1.Title = "Please locate the junction application"
-            If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
-                junctionpath = OpenFileDialog1.FileName
-            Else
-                Throw New Exception("Failure")
-            End If
-doexe:
+            Button2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If FolderBrowserDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            second = FolderBrowserDialog1.SelectedPath
+            Button3.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        Dim junctionpath As String = ""
+        OpenFileDialog1.Title = "Please locate the junction application"
+        If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            junctionpath = OpenFileDialog1.FileName
             Dim args As String = """" & second & """ """ & first & """"
             Dim pi As New ProcessStartInfo("cmd.exe", "/C junction.exe " & args) '(junctionpath, args)
             Dim output As String = ""
@@ -41,8 +36,9 @@ doexe:
             pi.WorkingDirectory = junctionpath.Replace("junction.exe", "")
             pi.RedirectStandardOutput = True
             p.StartInfo = pi
-
-
+            If Directory.Exists(second) Then
+                Directory.Delete(second, True)
+            End If
             If p.Start() Then
                 output = p.StandardOutput.ReadToEnd()
                 p.WaitForExit()
@@ -50,10 +46,8 @@ doexe:
             Else
                 MsgBox("Failure Starting")
             End If
-            Application.Exit()
-        Catch ex As Exception
-            MsgBox(ex.ToString())
-            Application.Exit()
-        End Try
+            
+        End If
+        
     End Sub
 End Class
